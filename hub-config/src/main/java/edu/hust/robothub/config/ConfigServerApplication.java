a9eca542af79d0d1917b71b4a260cd5e868853b1
zlber <1098294815@qq.com>
2020-09-28 00:35:27 +0800

    update config profile

--- hub-config/src/main/java/edu/hust/robothub/config/ConfigServerApplication.java
+++ hub-config/src/main/java/edu/hust/robothub/config/ConfigServerApplication.java
@@ -1,6 +1,5 @@
 package edu.hust.robothub.config;
 
-import edu.hust.robothub.core.robot.RobotManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -11,7 +10,6 @@ public class ConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
-		//RobotManager.getInstance().addRobot("test",10);
 	}
 
 }
